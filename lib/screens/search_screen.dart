9bce5f6a4cbd9189f69eec910cf1a7206eeb1adb
Nicky Huynh <98395576+NguyenNhatHuynh@users.noreply.github.com>
2024-09-10 22:23:30 +0700

    Search Screen

--- lib/screens/search_screen.dart
+++ lib/screens/search_screen.dart
@@ -1,9 +1,29 @@
 import 'package:flutter/material.dart';
 import 'package:weather_app_restapi/constants/text_styles.dart';
 import 'package:weather_app_restapi/views/gradient_container.dart';
+import 'package:weather_app_restapi/widgets/round_text_field.dart';
 
-class SearchScreen extends StatelessWidget {
+class SearchScreen extends StatefulWidget {
   const SearchScreen({super.key});
+
+  @override
+  State<SearchScreen> createState() => _SearchScreenState();
+}
+
+class _SearchScreenState extends State<SearchScreen> {
+  late final TextEditingController _controller;
+
+  @override
+  void initState() {
+    _controller = TextEditingController();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -19,6 +39,15 @@ class SearchScreen extends StatelessWidget {
             'Find the area or city that you want to know the detailed weather info at this time',
           ),
           const SizedBox(height: 40),
+          Row(
+            children: [
+              Expanded(
+                child: RoundTextField(
+                  controller: _controller,
+                ),
+              ),
+            ],
+          ),
         ],
       ),
     );
