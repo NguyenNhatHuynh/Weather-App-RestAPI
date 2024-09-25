590c53bdea9fb96f39bb0a750f3b0b88ebfa34a1
Nicky Huynh <98395576+NguyenNhatHuynh@users.noreply.github.com>
2024-09-25 11:07:37 +0700

    Design ForeCart View

---
+++ lib/views/weekly_forecast_view.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:weather_app_restapi/extensions/datetime.dart';
+import 'package:weather_app_restapi/providers/weekly_weather_provider.dart';
+
+class WeeklyForecastView extends ConsumerWidget {
+  const WeeklyForecastView({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final weeklyForecastData = ref.watch(weeklyWeatherProvider);
+
+    return weeklyForecastData.when(
+      data: (weeklyWeather) {
+        return ListView.builder(
+          itemCount: weeklyWeather.daily.weatherCode.length,
+          shrinkWrap: true,
+          physics: const NeverScrollableScrollPhysics(),
+          itemBuilder: (context, index) {
+            final dayOfWeek =
+                DateTime.parse(weeklyWeather.daily.time[index]).dayOfWeek;
+            final date = weeklyWeather.daily.time[index];
+            final temp = weeklyWeather.daily.temperature2mMax[index];
+            final icon = weeklyWeather.daily.weatherCode[index];
+          },
+        );
+      },
+      error: (error, stackTrace) {
+        return Center(
+          child: Text(error.toString()),
+        );
+      },
+      loading: () {
+        return const Center(
+          child: CircularProgressIndicator(),
+        );
+      },
+    );
+  }
+}
