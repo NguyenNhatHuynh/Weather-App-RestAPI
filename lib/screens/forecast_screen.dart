61cb75fd9b9efca94202a7bc01d922046a501c99
Nicky Huynh <98395576+NguyenNhatHuynh@users.noreply.github.com>
2024-09-26 08:45:51 +0700

    Weekly Forecast View

--- lib/screens/forecast_screen.dart
+++ lib/screens/forecast_screen.dart
@@ -1,52 +1,71 @@
 import 'package:flutter/material.dart';
-import 'package:weather_app_restapi/constants/text_styles.dart';
-import 'package:weather_app_restapi/extensions/datetime.dart';
-import 'package:weather_app_restapi/views/gradient_container.dart';
-import 'package:weather_app_restapi/views/hourly_forecast_view.dart';
+import '/constants/app_colors.dart';
+import '/constants/text_styles.dart';
+import '/extensions/datetime.dart';
+import '/views/gradient_container.dart';
+import '/views/hourly_forecast_view.dart';
+import '/views/weekly_forecast_view.dart';
 
-class ForecastScreen extends StatelessWidget {
-  const ForecastScreen({super.key});
+class ForecastReportScreen extends StatelessWidget {
+  const ForecastReportScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: GradientContainer(
-        children: [
-          const Align(
-              alignment: Alignment.center,
-              child: Text('Forecast Report', style: TextStyles.h1)),
-          const SizedBox(height: 40),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              const Text(
-                'Today',
-                style: TextStyles.h2,
-              ),
-              Text(
-                DateTime.now().dateTime,
-                style: TextStyles.subtitleText,
-              ),
-            ],
+    return GradientContainer(
+      children: [
+        // Page Title
+        const Align(
+          alignment: Alignment.center,
+          child: Text(
+            'Forecast Report',
+            style: TextStyles.h1,
           ),
-          const SizedBox(height: 20),
-          const HourlyForecastView(),
-          const SizedBox(height: 20),
-          const Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Text(
-                'Next Forecast',
-                style: TextStyles.h1,
-              ),
-              Icon(
-                Icons.calendar_month_outlined,
-                color: Colors.white,
-              ),
-            ],
-          )
-        ],
-      ),
+        ),
+
+        const SizedBox(height: 40),
+
+        // Today's date
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            const Text(
+              'Today',
+              style: TextStyles.h2,
+            ),
+            Text(
+              DateTime.now().dateTime,
+              style: TextStyles.subtitleText,
+            ),
+          ],
+        ),
+
+        const SizedBox(height: 20),
+
+        // Today's forecast
+        const HourlyForecastView(),
+
+        const SizedBox(height: 20),
+
+        // Next Forecast
+        const Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text(
+              'Next Forecast',
+              style: TextStyles.h2,
+            ),
+            Icon(
+              Icons.calendar_month_rounded,
+              color: AppColors.white,
+            ),
+          ],
+        ),
+
+        const SizedBox(height: 30),
+
+        // Weekly forecast
+        const WeeklyForecastView(),
+      ],
     );
   }
 }
--- lib/views/weekly_forecast_view.dart
+++ lib/views/weekly_forecast_view.dart
@@ -1,33 +1,47 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:weather_app_restapi/extensions/datetime.dart';
-import 'package:weather_app_restapi/providers/weekly_weather_provider.dart';
+
+import '/constants/app_colors.dart';
+import '/constants/text_styles.dart';
+import '/extensions/datetime.dart';
+import '/providers/get_weekly_forecast_provider.dart';
+import '/utils/get_weather_icons.dart';
+import '/widgets/subscript_text.dart';
 
 class WeeklyForecastView extends ConsumerWidget {
   const WeeklyForecastView({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final weeklyForecastData = ref.watch(weeklyWeatherProvider);
+    final weeklyForecast = ref.watch(weeklyForecastProvider);
 
-    return weeklyForecastData.when(
-      data: (weeklyWeather) {
+    return weeklyForecast.when(
+      data: (weatherData) {
         return ListView.builder(
-          itemCount: weeklyWeather.daily.weatherCode.length,
+          itemCount: weatherData.daily.weatherCode.length,
           shrinkWrap: true,
           physics: const NeverScrollableScrollPhysics(),
           itemBuilder: (context, index) {
             final dayOfWeek =
-                DateTime.parse(weeklyWeather.daily.time[index]).dayOfWeek;
-            final date = weeklyWeather.daily.time[index];
-            final temp = weeklyWeather.daily.temperature2mMax[index];
-            final icon = weeklyWeather.daily.weatherCode[index];
+                DateTime.parse(weatherData.daily.time[index]).dayOfWeek;
+            final date = weatherData.daily.time[index];
+            final temp = weatherData.daily.temperature2mMax[index];
+            final icon = weatherData.daily.weatherCode[index];
+
+            return WeeklyForecastTile(
+              date: date,
+              day: dayOfWeek,
+              icon: getWeatherIcon2(icon),
+              temp: temp.round(),
+            );
           },
         );
       },
       error: (error, stackTrace) {
         return Center(
-          child: Text(error.toString()),
+          child: Text(
+            error.toString(),
+          ),
         );
       },
       loading: () {
@@ -35,6 +49,70 @@ class WeeklyForecastView extends ConsumerWidget {
           child: CircularProgressIndicator(),
         );
       },
+    );
+  }
+}
+
+class WeeklyForecastTile extends StatelessWidget {
+  const WeeklyForecastTile({
+    super.key,
+    required this.day,
+    required this.date,
+    required this.temp,
+    required this.icon,
+  });
+
+  final String day;
+  final String date;
+  final int temp;
+  final String icon;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.symmetric(
+        horizontal: 16,
+        vertical: 20,
+      ),
+      margin: const EdgeInsets.symmetric(
+        vertical: 12,
+      ),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(15),
+        color: AppColors.accentBlue,
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Column(
+            children: [
+              Text(
+                day,
+                style: TextStyles.h3,
+              ),
+              const SizedBox(height: 5),
+              Text(
+                date,
+                style: TextStyles.subtitleText,
+              ),
+            ],
+          ),
+
+          // Temperature
+          SuperscriptText(
+            text: '$temp',
+            color: AppColors.white,
+            superScript: '°C',
+            superscriptColor: AppColors.white,
+          ),
+
+          // weather icon
+          Image.asset(
+            icon,
+            width: 60,
+          ),
+        ],
+      ),
     );
   }
 }
